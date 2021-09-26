3952b24fa4a09b06b338c2a474bc3553d0b5a71a
davikrig-ha <daviluia2@gmail.com>
2021-09-26 11:21:26 -0300

    variaveis

--- variaveis/js/script.js
+++ variaveis/js/script.js
@@ -1,4 +1,28 @@
-alert('teste');
+//string
 
-//pegando o valor do input e colocando um texto nele
-document.getElementById('text').value = 'teste';
+var texto = 'JavaScript';
+
+//nunber
+
+var numeroInteiro = 8;
+var numeroFloat = 8.5;
+
+//boolean
+
+var teste = false 
+
+
+/*JavaScript é uma linguagem de tipagem  dinamica,
+o tipo da variavel é atribuido de acordo com o 
+valor atribuido na mesma*/ 
+
+//abrir um dialog
+
+alert(texto); 
+
+//escrever o conteudo da variavel na pagina
+
+document.write(texto);
+
+//debug 
+console.log(texto);
